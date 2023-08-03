add06354931a00fe030dc3d9a7e9ff82ff3384b3
easton683 <easton.jackson683@gmail.com>
2023-08-03 15:44:55 -0600

    Array and ListStack

--- test.java
+++ test.java
@@ -13,6 +13,121 @@ import java.nio.file.Path;
 public class test {
 	public static int counter = 0;
 public static void main(String[] args) {
+	int[] hundred = new int[100];
+	int[] thousand = new int[1000];
+	int[] tenThousand = new int[10000];
+	int[] hundredThousand = new int[100000];
+	for (int i = 0; i < hundred.length; i++) {
+		hundred[i] = (int)Math.random()*10;
+	}
+	for (int i = 0; i < thousand.length; i++) {
+		thousand[i] = (int)Math.random()*10;
+	}
+	for (int i = 0; i < tenThousand.length; i++) {
+		tenThousand[i] = (int)Math.random()*10;
+	}
+	for (int i = 0; i < hundredThousand.length; i++) {
+		hundredThousand[i] = (int)Math.random()*10;
+	}
+	long begin;
+	long end;
+	System.gc();
+	ArrayStack arrStack = new ArrayStack(100);
+	begin = System.nanoTime();	
+	for (int i = 0; i < hundred.length; i++) {
+		arrStack.push(hundred[i]);
+	}
+	for (int i = 0; i < hundred.length; i++) {
+		arrStack.pop();
+	}
+	end = System.nanoTime();
+	System.out.println("arrStack");
+	System.out.println("100 - " + (end-begin));
+
+	System.gc();
+	ArrayStack arrStack1 = new ArrayStack(1000);
+	begin = System.nanoTime();
+	for (int i = 0; i < thousand.length; i++) {
+		arrStack1.push(thousand[i]);
+	}
+	for (int i = 0; i < thousand.length; i++) {
+		arrStack1.pop();
+	}
+	end = System.nanoTime();
+	System.out.println("1000 - " + (end-begin));
+
+	System.gc();
+	ArrayStack arrStack2 = new ArrayStack(1000);
+	begin = System.nanoTime();
+	for (int i = 0; i < tenThousand.length; i++) {
+		arrStack2.push(tenThousand[i]);
+	}
+	for (int i = 0; i < tenThousand.length; i++) {
+		arrStack2.pop();
+	}
+	end = System.nanoTime();
+	System.out.println("10000 - " + (end-begin));
+
+	System.gc();
+	ArrayStack arrStack3 = new ArrayStack(1000);
+	begin = System.nanoTime();
+	for (int i = 0; i < hundredThousand.length; i++) {
+		arrStack3.push(hundredThousand[i]);
+	}
+	for (int i = 0; i < hundredThousand.length; i++) {
+		arrStack3.pop();
+	}
+	end = System.nanoTime();
+	System.out.println("100000 - " + (end-begin));
+
+	System.out.println("listStack:");
+	System.gc();
+	ListStack lStack = new ListStack<>();
+	begin = System.nanoTime();
+	for (int i = 0; i < hundred.length; i++) {
+		lStack.push(hundred[i]);
+	}
+	for (int i = 0; i < hundred.length; i++) {
+		lStack.pop();
+	}
+	end = System.nanoTime();
+	System.out.println("100 - " + (end-begin));
+
+	System.gc();
+	ListStack lStack1 = new ListStack<>();
+	begin = System.nanoTime();
+	for (int i = 0; i < thousand.length; i++) {
+		lStack1.push(thousand[i]);
+	}
+	for (int i = 0; i < thousand.length; i++) {
+		lStack1.pop();
+	}
+	end = System.nanoTime();
+	System.out.println("1000 - " + (end-begin));
+
+	System.gc();
+	ListStack lStack2 = new ListStack<>();
+	begin = System.nanoTime();
+	for (int i = 0; i < tenThousand.length; i++) {
+		lStack2.push(tenThousand[i]);
+	}
+	for (int i = 0; i < tenThousand.length; i++) {
+		lStack2.pop();
+	}
+	end = System.nanoTime();
+	System.out.println("100 - " + (end-begin));
+
+	System.gc();
+	ListStack lStack3 = new ListStack<>();
+	begin = System.nanoTime();
+	for (int i = 0; i < hundredThousand.length; i++) {
+		lStack3.push(hundredThousand[i]);
+	}
+	for (int i = 0; i < hundredThousand.length; i++) {
+		lStack3.pop();
+	}
+	end = System.nanoTime();
+	System.out.println("100000 - " + (end-begin));
 
 }
 
