389e92c3498688c457a9664277b345f852d7a460
Easton683 <easton.jackson683@gmail.com>
2023-08-14 21:11:44 -0600

    Chapter 19

--- Assignment_19.java
+++ Assignment_19.java
@@ -10,12 +10,21 @@ public class Assignment_19 {
 	//Non-recursive implementation adapted from book - but I put in the { } as you obviously should :) 
 	//TODO: Screenshot recursive method for your report as this method currently works great so the tester can't tell if you actually used recursion
 	private static <AnyType extends Comparable<? super AnyType>> BinaryNode<AnyType> findMin(BinaryNode<AnyType> t) {
-		if( t != null ) {
+		/*if( t != null ) {
 			while( t.left != null ) {
 				t = t.left;
 			}
 		}
+		return t;*/
+
+		if (t==null){
+			throw new ItemNotFoundException();
+		}else if(t.left != null) {
+			t.left = findMin(t.left);
+			return t.left;
+		}else {
 		return t;
+		}
 	}
 
 	
@@ -28,12 +37,19 @@ public class Assignment_19 {
 	//Non-recursive implementation adapted from book - but I put in the { } as you obviously should :) 
 	//TODO: Screenshot recursive method for your report as this method currently works great so the tester can't tell if you actually used recursion
 	private static <AnyType extends Comparable<? super AnyType>> BinaryNode<AnyType> findMax(BinaryNode<AnyType> t) {
-		if( t != null ) {
+		/*if( t != null ) {
 			while( t.right != null ) {
 				t = t.right;
 			}
 		}
-		return t;
+		return t;*/
+		if (t==null) {
+			throw new ItemNotFoundException();
+		}else if (t.right != null){
+			return findMax(t.right);
+		}else {
+			return t;
+		}
 	}
 
 	//19.15
@@ -45,7 +61,7 @@ public class Assignment_19 {
 	//Non-recursive implementation adapted from book - but I put in the { } as you obviously should :) 
 	//TODO: Screenshot recursive method for your report as this method currently works great so the tester can't tell if you actually used recursion
 	private static <AnyType extends Comparable<? super AnyType>> BinaryNode<AnyType> find(AnyType x, BinaryNode<AnyType> t) {
-		while( t != null ){
+		/*while( t != null ){
 			if( x.compareTo( t.element ) < 0 ) {
 				t = t.left;
 			}
@@ -56,7 +72,17 @@ public class Assignment_19 {
 				return t; // Match
 			}
 		}
-		return null; // Not found
+		return null; // Not found*/
+		if (t==null) {
+			return null;
+		}
+		if (x.equals(t.element)){
+			return t;
+		}else if (x.compareTo(t.element)>0){
+			return find(x, t.right);
+		}else{
+			return find(x, t.left);
+		}
 	}
 	
 	
@@ -73,7 +99,25 @@ public class Assignment_19 {
 	//so the screenshot of no errors in the main is all I really need
 	private static <AnyType extends Comparable<? super AnyType>> String toString(BinaryNode<AnyType> t) {
 		//Note, if you are going to use recursion, use the methods calls like toString(t) instead of t.toString() as you would if this method was inside the BinaryNode class
-		return null; 
+		if (t==null) {
+			return "";
+		}
+		StringBuilder builder = new StringBuilder();
+		builder.append(toString(t.left));
+		builder.append(t.element);
+		builder.append(", ");
+		builder.append(toString(t.right));
+		return builder.toString().substring(0, builder.toString().length());
+		/*String result = "";
+		result += toString(t.left);
+		if (t.right!=null) {
+		result += t.element + ", ";			
+		}else{
+			result +=t.element;
+		}
+
+		result += toString(t.right);
+		return result;*/
 	}
 
 }
